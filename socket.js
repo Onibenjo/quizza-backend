3d7ec4557f26a2896b89ccd2ca1f90c95e0dc0de
cykic <cyb3rcykic@gmail.com>
2021-12-10 17:52:37 +0000

    fix: dded

--- socket.js
+++ socket.js
@@ -10,7 +10,7 @@ class Socket {
     const options = {
       transports: ["polling"],
       cors: {
-        origin: "http:localhost:3000",
+        origin: ["http:localhost:3000", "*"],
       },
     };
 
